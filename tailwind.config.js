46637a3887cb5a7fdaa4530cefa84b955e66b355
Ritik26052000 <sonwanianshu2000@gmail.com>
2024-05-04 17:11:07 +0530

    tailwind assignment 3

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      color:{
+        'pri':'#b7b8e5',
+        'sec': ' #b7dee6',
+        'tre': ' #aec6f4',
+      },
+    },
+  },
+  plugins: [],
+}
+
